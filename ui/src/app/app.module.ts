253a7f8f69ffab8a56cfca1a2b22a9b1235fbf2a
BlueLight268 <98980709+BlueLight268@users.noreply.github.com>
2023-02-04 03:44:16 +0100

    #2 | added ui microservice, basic routing | nr: ng-bootstrap, frog icon

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterBusinessComponent } from './register-business/register-business.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterBusinessComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
